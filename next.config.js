06bcb216656a9f8d13b32b24785788fef3e0b7ce
Jacob Liberty <jacob.tobin.liberty@gmail.com>
2025-05-05 16:08:17 -0400

    fix next config

--- next.config.js
+++ next.config.js
@@ -1,21 +1,16 @@
 /** @type {import('next').NextConfig} */
 const basePath = process.env.NEXT_PUBLIC_BASE_PATH || process.env.BASE_PATH || '';
-const assetPrefix = process.env.NEXT_PUBLIC_BASE_PATH || process.env.ASSET_PREFIX || '';
 
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
   output: 'export',
   basePath,
-  assetPrefix,
-  trailingSlash: true,
+  assetPrefix: basePath,
   images: {
     unoptimized: true,
     dangerouslyAllowSVG: true,
     contentDispositionType: 'attachment',
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
-  distDir: 'out',
 };
 
-module.exports = nextConfig; 
+export default nextConfig;
