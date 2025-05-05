582c5bf94caa45118282a587f77ba016d5fac4d5
Jacob Liberty <jacob.tobin.liberty@gmail.com>
2025-05-05 16:19:35 -0400

    base path change

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
-const basePath = process.env.NEXT_PUBLIC_BASE_PATH || process.env.BASE_PATH || '';
 
 const nextConfig = {
   output: 'export',
-  basePath,
-  assetPrefix: basePath,
+  basePath: "portfolio",
+  assetPrefix: "portfolio",
   images: {
     unoptimized: true,
     dangerouslyAllowSVG: true,
